d91b1aead1fdcef7bcf841551f3ab475c3f676a9
Henry Borchers <henryborchers@yahoo.com>
2016-07-16 15:19:17 -0500

    added test suite

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
     install_requires=depends,
     description='Checks to see if the internet is still accessible and let\'s you know if you know if it goes down',
     zip_safe=False,
+    test_suite="tests",
+    test_require=['nose'],
     entry_points={
         'console_scripts': [
             'checkinternet = checkInternet.__main__:main'
