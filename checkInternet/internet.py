4f51d371b9d97679355a89bb9724f9d7fa751336
Henry Borchers <henryborchers@yahoo.com>
2016-07-16 14:41:05 -0500

    Added multiple attempts

--- checkInternet/internet.py
+++ checkInternet/internet.py
@@ -1,10 +1,26 @@
 
 from urllib import request
 
-def internet_on():
+import time
+
+
+def internet_on(timeout=2):
     try:
-        response=request.urlopen('http://www.google.com',timeout=1)
+        response = request.urlopen('http://www.google.com', timeout=timeout)
         return True
     except request.URLError as err: pass
     return False
 
+def internet_fail():
+    return False
+
+
+def multi_attempt(trys=3, timeout=2):
+    for _ in range(trys):
+        if internet_on(timeout=timeout):
+            return True
+        else:
+            time.sleep(10)
+            continue
+
+    return False
