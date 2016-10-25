4b3747e8c7b48464013d8148a7f10e07641712e8
Henry Borchers <henryborchers@yahoo.com>
2016-10-24 20:11:27 -0500

    brought important modules closer

--- checkInternet/Alerts/__init__.py
+++ checkInternet/Alerts/__init__.py
@@ -1,1 +1,7 @@
-# from .alert import Alert
+from .tk_alerter import TkAlerter as tk
+try:
+    from .coco_alerter import CocoAlerter as coco
+except ImportError:
+    pass
+from .abs_alerters import AbsAlert
+from .alert_types import AlertTypes
