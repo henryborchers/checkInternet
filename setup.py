f4484b126417d4739806bb17a7be2ca44053b189
Henry Borchers <henryborchers@yahoo.com>
2016-10-24 20:09:14 -0500

    new version

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ if sys.platform == "darwin":
 
 setup(
     name='CheckInternet',
-    version='0.2.9',
+    version='0.2.10',
     packages=['checkInternet', 'checkInternet.Alerts'],
     url='',
     license='',
     author='Henry Borchers',
     author_email='',
+    scripts=['scripts/cli.py'],
     install_requires=depends,
     description='Checks to see if the internet is still accessible and let\'s you know if you know if it goes down',
     zip_safe=False,
@@ -20,7 +21,7 @@ setup(
     test_require=['nose'],
     entry_points={
         'console_scripts': [
-            'checkinternet = checkInternet.__main__:main'
+            'checkinternet = scripts:main'
         ]
     }
 )
